dc3137e4e0a6ecc526249f288e07f04457db6583
ximik-xim <45805384+ximik-xim@users.noreply.github.com>
2023-07-15 00:13:22 +0300

    Создание скрипта для определения поворота отображаймого изображения на телефоне Смена платформы для сборки на андроид Добавление плагина Android Logcat (разблокирование) для просмотров логов с телефона

---
+++ Assets/Scripts/Test/PositionDisplayedImage.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PositionDisplayedImage : MonoBehaviour
+{
+
+ public static PositionDisplayedImage PositionDisplayedImageStat;
+ 
+ public event Action OnInit;
+
+ public ScreenOrientation CurrentPositionDisplayedImage => _currentPositionDisplayedImage;
+ private ScreenOrientation _currentPositionDisplayedImage;
+ public event Action<ScreenOrientation> OnRotateDisplayedImage;
+
+ [SerializeField] 
+ private bool _setDontDestroy = true;
+ 
+ private void Awake()
+ {
+  PositionDisplayedImageStat = this;
+  OnInit?.Invoke();
+
+  if (_setDontDestroy == true)
+  {
+   DontDestroyOnLoad(gameObject); 
+  }
+  
+ }
+
+ public void SetOrientation(ScreenOrientation orientation)
+ {
+  if (orientation != CurrentPositionDisplayedImage)
+  {
+   Screen.orientation = orientation;
+   return;
+  }
+  
+  Debug.LogError(orientation+" уже была выбранна в виде текущей ориентации");
+  
+ }
+ 
+ private void Update()
+ {
+
+  if (CurrentPositionDisplayedImage != Screen.orientation)
+  {
+   OnRotateDisplayedImage?.Invoke(Screen.orientation);
+
+   _currentPositionDisplayedImage = Screen.orientation;
+
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  Debug.Log("In OR = "+Input.deviceOrientation);
+  Debug.Log("Sc OR = "+Screen.orientation);
+
+
+//Проверено тестами
+//Input.deviceOrintation возращает текущее положение телефона в пространстве
+
+//Screen.orientation возращает текущий режим отображения на телефоне
+
+// Они могут спокойно не совподать между собой,
+// к примеру
+// Если отключить автоповорот в Портретном режиме и перевенуть его, то картинка как отображалась в Портретном режиме,
+// так и будет отображаться в Портретном режиме, хотя телефон находиться Ланшафтном положении
+// Соответственно в такой ситуации 
+//  Screen.orientation - вернет Портретный режим отображения телефона
+//  Input.deviceOrientation - вернет Ланшафтном положение телефона
+
+// Так же к примеру значения не будут совпадаться, если отключить в Unity в Player Setting некоторые режими отображения
+// Тогда они будут отключены для Screen.orientation и по этому телефон сам не будет в них переходит,
+// и по этому значения будут отличаться между Input.deviceOrientation(положением телефона в пространстве) и Screen.orientation(режим отображения изображения в телефоне)
+ } 
+
+ 
+ 
+}
